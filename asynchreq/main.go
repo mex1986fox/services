1652c6d9cbf7a1963ff9cdf9052082c9f269c25c
Maxim <mex1986fox@mail.ru>
2019-03-10 21:18:27 +0700

    asynchreq restyling

--- asynchreq/main.go
+++ asynchreq/main.go
@@ -9,63 +9,104 @@ import (
 	"github.com/gorilla/mux"
 )
 
+// тип асинхронного запроса
 type AsynchRequest struct {
 	Host string
-	Port string
 	Url  string
 	Data string
 }
 
-var PullAR []AsynchRequest = []AsynchRequest{}
+// // пул асинхронных запросов
+// type PullAsynchRequest []AsynchRequest
+
+// пул каналов очередей
+// type PullTurn map[string]chan AsynchRequest
+type PullChanAR map[string]chan AsynchRequest
+
+var pullChanAR PullChanAR = PullChanAR{}
+
+//для блокировке потоков при записи в PullAR
+//var PullARMutex = &sync.RWMutex{}
 
 func requestCreate(w http.ResponseWriter, r *http.Request) {
 
 	host := r.URL.Query().Get("host")
-	port := r.URL.Query().Get("port")
 	url := r.URL.Query().Get("url")
 	data := r.URL.Query().Get("data")
-	// создать массив запросов
+	// создать запрос
 	asynchRequest := AsynchRequest{
 		Host: host,
-		Port: port,
 		Url:  url,
 		Data: data,
 	}
-	PullAR = append(PullAR, asynchRequest)
+
+	// проверить существование очереди
+	if _, ok := pullChanAR[asynchRequest.Host]; ok {
+		//если есть такая очередь
+		fmt.Println("\r\t...добавил новый запрос в существующую горутину..\r\t")
+		pullChanAR[asynchRequest.Host] <- asynchRequest
+
+	} else {
+		//если нет
+		// создать пул очередь запросов
+		var _chanAR = make(chan AsynchRequest, 5)
+		pullChanAR[asynchRequest.Host] = _chanAR
+		// создать горутину
+		// которая будет обрабатывать очередь
+		go TransleterAR(pullChanAR[asynchRequest.Host])
+		pullChanAR[asynchRequest.Host] <- asynchRequest
+		fmt.Println("\r\t..............создал новую горутину .............\r\t")
+	}
+
 	// //vars := mux.Vars(r)
 	// ip := r.URL.Query().Get("ip")
-	b, _ := json.Marshal(PullAR)
+	fmt.Println(pullChanAR)
+	b, _ := json.Marshal(pullChanAR)
 	response := fmt.Sprint(string(b))
 	fmt.Fprint(w, response)
 
 }
 
-func TransleterAR() {
+var count int = 0
+
+func TransleterAR(chanAR chan AsynchRequest) {
+	var pullAR = []AsynchRequest{}
 	for {
-		if len(PullAR) >= 1 {
-			asReq := PullAR[0]
+		// если пул очереди запросов не пустой
+		// копируем запросы в очередь
+		if len(chanAR) >= 1 {
+			fmt.Println("\r\t..не пуста.....\r\t")
+
+			for i := 0; i < len(chanAR); i++ {
+				//fmt.Println(cAR)
+				pullAR = append(pullAR, <-chanAR)
+			}
+		}
+
+		// если есть запросы обрабатываем их
+		if len(pullAR) >= 1 {
+			asReq := pullAR[0]
 			data := []byte(asReq.Data)
 			r := bytes.NewReader(data)
-			resp, err := http.Post(fmt.Sprint("http://", asReq.Host, ":", asReq.Port, asReq.Url), "application/json", r)
+			resp, err := http.Post(fmt.Sprint(asReq.Host, asReq.Url), "application/json", r)
 			if err != nil {
-				fmt.Println(err)
+				fmt.Println("Выполнил запрос с ошибкой: ", err)
 
 			} else {
 				bs := make([]byte, 1014)
 				n, _ := resp.Body.Read(bs)
 				fmt.Println(string(bs[:n]))
-				fmt.Println("Выполнил запрос: ", "http://", asReq.Host, ":", asReq.Port, asReq.Url)
+				fmt.Println("Выполнил запрос: ", asReq.Host, asReq.Url)
 				resp.Body.Close()
-
 			}
-			PullAR = PullAR[1:]
+			pullAR = pullAR[1:]
 		}
 	}
-
 }
 
 func main() {
-	go TransleterAR()
+	// запустить горутину
+	// которая будет запросы отправлять
 	fmt.Println("горутина стартанула")
 	router := mux.NewRouter()
 	router.HandleFunc("/api/request/create", requestCreate)
@@ -73,8 +114,5 @@ func main() {
 	fmt.Println("Server is listening...")
 
 	http.ListenAndServe(":8181", nil)
-
-	// запустить горутину
-	// которая будет запросы отправлять
 
 }
